7204891df2aa8e60dec9c275f8e547de66d80122
Valery Rokka <bassist68@gmail.com>
2024-11-03 13:39:11 +0100

    Task#43 Scheduler for chat polls Validation logic added with tests

--- telegram/src/main/java/by/mrrockka/validation/poll/PollMessageValidator.kt
+++ telegram/src/main/java/by/mrrockka/validation/poll/PollMessageValidator.kt
@@ -1,11 +1,17 @@
 package by.mrrockka.validation.poll
 
 import by.mrrockka.domain.PollTask
+import by.mrrockka.exception.BusinessException
 import org.springframework.stereotype.Component
 
 @Component
 class PollMessageValidator {
     fun validatePoll(pollTask: PollTask) {
-        throw UnsupportedOperationException("Not supported yet.")
+        when {
+            pollTask.message.isBlank() -> throw PollHasNoRequiredFieldException("message")
+            pollTask.options.isEmpty() -> throw PollHasNoRequiredFieldException("options")
+        }
     }
 }
+
+internal class PollHasNoRequiredFieldException(private val field: String) : BusinessException("Poll $field is required.")
