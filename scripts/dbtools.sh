902c49babd8527bb18dd7b95595782c329ab9c56
mrrockka <mrrockka@Valerys-MacBook-Pro.local>
2025-09-28 15:49:48 +0200

    setting up pipeline

--- scripts/dbtools.sh
+++ scripts/dbtools.sh
@@ -4,9 +4,9 @@ dbBackup(){
 #  keep not more then 3 backups
   pg_dump -U psuser -d pokerclock -F tar -f "$HOME/backup/pokerclock.$(date '+%d-%m-%Y').tar"
 
-  fileSize=$(find ~/backup/ -type f | wc -l)
+  fileSize=$(find ~/backup/ -type f | wc -l)-3
 
-  find ~/backup/ -type f -printf "%Cx.%CX %p\n" | sort -n | awk '{print $3}' | head -"$($fileSize-3)" | xargs -0 rm
+  find ~/backup/ -type f -printf "%Cx.%CX %p\n" | sort -n | awk '{print $3}' | head -$fileSize | xargs -0 rm
 }
 
 dbBackup
