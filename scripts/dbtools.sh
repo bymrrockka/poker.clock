40a07540b036b322363edeef08e7ade49d7a6f7d
mrrockka <mrrockka@Valerys-MacBook-Pro.local>
2025-10-02 15:43:10 +0200

    Run script fix

--- scripts/dbtools.sh
+++ scripts/dbtools.sh
@@ -7,7 +7,7 @@ dbBackup(){
 
   if [ "$fileSize" -gt 0 ]; then
     echo "$fileSize old backups removed"
-    find ~/backup/ -type f -printf "%Cx.%CX %p\n" | sort -n | awk '{print $3}' | head -$fileSize | xargs -0 rm
+    find ~/backup/ -type f -printf "%Cx.%CX %p\n" | sort -n | awk '{print $2}' | head -$fileSize | xargs -0 rm
   else
     echo "Backups contain less than 3 files"
   fi
