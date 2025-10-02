4acdaeaaddd7fe3874c1b00ca1861f3142fe6f7e
mrrockka <mrrockka@Valerys-MacBook-Pro.local>
2025-10-02 23:30:26 +0200

    Uprocessed handler updated to assert only on commands

--- telegram/src/main/java/by/mrrockka/commands/UnprocessedCommandHandler.kt
+++ telegram/src/main/java/by/mrrockka/commands/UnprocessedCommandHandler.kt
@@ -4,6 +4,7 @@ import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.UnprocessedHandler
 import eu.vendeli.tgbot.api.message.sendMessage
 import eu.vendeli.tgbot.types.component.MessageUpdate
+import eu.vendeli.tgbot.types.msg.EntityType
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,7 +14,11 @@ class UnprocessedCommandHandler(
 
     @UnprocessedHandler
     suspend fun unprocessed(message: MessageUpdate?) {
-        if (message != null)
-            sendMessage { "Can't process command" }.send(message.message.chat.id, bot)
+        message?.message?.entities
+                ?.find { it.type == EntityType.BotCommand }
+                ?.also { command ->
+                    sendMessage { "Can't process command $command" }
+                            .send(message.message.chat.id, bot)
+                }
     }
 }
