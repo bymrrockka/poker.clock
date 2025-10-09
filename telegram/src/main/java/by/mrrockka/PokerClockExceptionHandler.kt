d0b4980c995181d0eac28d1023bf17b023061d97
mrrockka <mrrockka@Valerys-MacBook-Pro.local>
2025-10-09 12:39:32 +0200

    Task#54 Pin and remove bot messages experimenting with actions

--- telegram/src/main/java/by/mrrockka/PokerClockExceptionHandler.kt
+++ telegram/src/main/java/by/mrrockka/PokerClockExceptionHandler.kt
@@ -4,21 +4,26 @@ import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.api.message.sendMessage
 import eu.vendeli.tgbot.interfaces.helper.ExceptionHandler
 import eu.vendeli.tgbot.types.component.ProcessedUpdate
-import org.slf4j.helpers.Reporter.warn
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val logger = KotlinLogging.logger { }
 
 object PokerClockExceptionHandler : ExceptionHandler {
     override suspend fun handle(exception: Throwable, update: ProcessedUpdate, bot: TelegramBot) {
         val chatid = update.origin.message?.chat?.id
 
         if (chatid == null) {
-            warn("Update chat id is missing")
+            logger.warn { "Update chat id is missing" }
             return
         }
 
         val message = when (exception) {
             is IllegalStateException -> exception.message
-            else -> "Exception occurred during handling"
-        } ?: "Exception occurred during handling"
+            else -> {
+                logger.error(exception) { "" }
+                "Exception occurred during handling"
+            }
+        } ?: "No exception message specified"
         sendMessage { message }.send(chatid, bot)
     }
 }
