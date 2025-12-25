84098e137dce2bb8e48145d0de7210ea68d772d2
Valery <bassist68@gmail.com>
2025-12-25 14:57:29 +0100

    Task#63 My stat fix (#66)
    * Task#63 My stat fix
    More informative errors messages
    
    * Task#63 My stat fix
    Moved summaries storage on calculate service
    
    ---------
    
    Co-authored-by: Valery Rokka <mrrockka@gmail.com>

--- core/src/main/java/by/mrrockka/service/CalculationService.kt
+++ core/src/main/java/by/mrrockka/service/CalculationService.kt
@@ -5,7 +5,9 @@ import by.mrrockka.domain.CashGame
 import by.mrrockka.domain.Game
 import by.mrrockka.domain.Payout
 import by.mrrockka.domain.TournamentGame
+import by.mrrockka.domain.toSummary
 import by.mrrockka.repo.GameRepo
+import by.mrrockka.repo.GameSummaryRepo
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
@@ -19,11 +21,16 @@ interface CalculationService {
 open class CalculationServiceImpl(
         private val calculator: GameCalculator,
         private val gameRepo: GameRepo,
+        private val gameSummaryRepo: GameSummaryRepo,
 ) : CalculationService {
 
     @Transactional(propagation = Propagation.REQUIRED)
     override fun calculate(game: Game): List<Payout> {
         val payouts = calculator.calculate(game)
+
+        val gameSummaries = game.toSummary()
+        gameSummaryRepo.store(game.id, gameSummaries)
+
         if (game.finishedAt == null) {
             gameRepo.store(game.finish())
         }
