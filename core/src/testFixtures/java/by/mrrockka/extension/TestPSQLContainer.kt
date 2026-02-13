6ceca860acc78285f1690ff785999eb3643ddc80
Valery Rokka <mrrockka@gmail.com>
2026-02-13 18:03:22 +0100

    Github action fix Updated test containers because of compatibility with new ubuntu Ubuntu version is fixed now

--- core/src/testFixtures/java/by/mrrockka/extension/TestPSQLContainer.kt
+++ core/src/testFixtures/java/by/mrrockka/extension/TestPSQLContainer.kt
@@ -1,17 +1,16 @@
 package by.mrrockka.extension
 
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.utility.DockerImageName
+import org.testcontainers.postgresql.PostgreSQLContainer
 
-class TestPSQLContainer : PostgreSQLContainer<TestPSQLContainer>(DockerImageName.parse("postgres:$version")) {
-    //  todo: found out that testcontainer creates two containers, one with default values another one with overriden - needs investigation
+class TestPSQLContainer {
+
     companion object {
         const val version: String = "16.1"
         const val dbName: String = "pokerclock"
         const val username = "itest"
         const val password = "itest123"
 
-        val container: TestPSQLContainer = TestPSQLContainer()
+        val container = PostgreSQLContainer("postgres:$version")
                 .withDatabaseName(dbName)
                 ?.withUsername(username)
                 ?.withPassword(password)
